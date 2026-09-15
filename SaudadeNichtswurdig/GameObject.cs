169b658e3e7890c8c52cc91526c92c5d0dc9a236
WilliamOfWoodstock <229272274+WilliamofWoodstock@users.noreply.github.com>
2026-09-14 21:26:19 -0400

    Initial ground work
    - GameObject ist cool & player has some stuff but I need the sprites.

---
+++ SaudadeNichtswurdig/GameObject.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaudadeNichtswurdig
+{
+    /// <summary>
+    /// Basic common infrastructure shared by all game objects
+    /// </summary>
+    internal abstract class GameObject
+    {
+        protected Texture2D texture;
+        private Rectangle box;
+        /// <summary>
+        /// The rectangle that contains the object's sprite
+        /// </summary>
+        public Rectangle Box { get => box; set => box = value; }
+
+        /// <summary>
+        /// The object's x position (based on their top left corner)
+        /// </summary>
+        public int X { get => box.X; set => box.X = value; }
+
+        /// <summary>
+        /// Creates a GameObject with the most basic information filled out
+        /// </summary>
+        /// <param name="texture">Texture being used to represent the GameObject</param>
+        public GameObject(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+        /// <summary>
+        /// Creates a GameObject with the most basic information filled out
+        /// </summary>
+        /// <param name="texture">Texture being used to represent the GameObject</param>
+        /// <param name="box">Rectangle that will house their primary texture</param>
+        public GameObject(Texture2D texture, Rectangle box)
+        {
+            this.texture = texture; this.box = box;
+        }
+        /// <summary>
+        /// Creates a GameObject with the most basic information filled out
+        /// </summary>
+        /// <param name="texture">Texture being used to represent the GameObject</param>
+        /// <param name="x">Starting x position of object (top left corner of sprite)</param>
+        /// <param name="y">Starting y position of object (top left corner of sprite)</param>
+        /// <param name="width">In-game width of sprite</param>
+        /// <param name="height">In-game Height of sprite</param>
+        public GameObject(Texture2D texture, int x, int y, int width, int height)
+        {
+            this.texture = texture; box = new Rectangle(x, y, width, height);
+        }
+
+        public abstract void Update(GameTime gameTime);
+    }
+}
---
+++ SaudadeNichtswurdig/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace SaudadeNichtswurdig
+{
+    enum PlayerStates
+    {
+        FaceLeft,
+        FaceRight
+    }
+
+    internal class Player : GameObject
+    {
+        int widthOfSingleSprite;
+        public Player(Texture2D texture, int x, int y, int width, int height) : base(texture)
+        {
+            Box = new Rectangle(x, y, width, height);
+        }
+        public override void Update(GameTime gameTime)
+        {
+            KeyboardState input = Keyboard.GetState(); 
+        }
+    }
+}
